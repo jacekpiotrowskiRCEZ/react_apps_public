0a7646d7482eca02470351992095015822e819aa
jacekpiotrowskiRCEZ <76050734+jacekpiotrowskiRCEZ@users.noreply.github.com>
2023-10-03 08:43:05 +0200

    Kamień Papier Nożyce

--- rps/src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <div className='footer'>
-      <h5>Autor: JP </h5>
-  </div>;
-};
-
-export default Footer;
--- rps/src/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-  return <div className='Header'>
-      <h1>Kamień Papier Nożyce</h1>
-  </div>;
-};
-
-export default Header;
