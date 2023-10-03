4251b5cf8f0c11cf556bbe22e08707b80bb2f44b
jacekpiotrowskiRCEZ <76050734+jacekpiotrowskiRCEZ@users.noreply.github.com>
2023-10-03 08:32:11 +0200

    v1

---
+++ rps/src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+  return <div className='footer'>
+      <h5>Autor: JP </h5>
+  </div>;
+};
+
+export default Footer;
---
+++ rps/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return <div className='Header'>
+      <h1>Kamień Papier Nożyce</h1>
+  </div>;
+};
+
+export default Header;
